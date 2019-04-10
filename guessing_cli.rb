0df28a303cbfdcca3e6d888c10979d1c8395c2b0
DaNeil Coulthard <caffiendkitten@gmail.com>
2019-04-10 20:52:35 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -14,7 +14,7 @@ def run_guessing_game
       puts "Goodbye!"
       break
     else
-      puts "/The computer guessed 5./"
+      #puts "/The computer guessed 5./"
       break
       #guess = gets.chomp
     end
